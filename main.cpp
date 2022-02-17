430ccd5a4a7fab3ee499e00791c1178525dfe47b
Anton Ilyasov <ilyasov.toni.97@mail.ru>
2022-02-18 00:45:56 +0300

    test main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int check(int got, int expected)
+{
+	if (got != expected)
+	{
+		return 1;
+	}
+}
+
+int summ(int a, int b)
+{
+	return a + b;
+}
+
+
+int main()
+{
+	int result = 0;
+	result = check(summ(5, 5), 10);
+	result = check(summ(2, 5), 7);
+}
